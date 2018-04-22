d96fa1f8d469839567e9be484e24791d0cb66748
nd <nitin.rajen.deshmukh@gmail.com>
2018-04-22 23:00:38 +0200

    Cleaned up serverless.yml and updated handler.js to use Async/Await

--- handler.js
+++ handler.js
@@ -1,16 +1,17 @@
-'use strict';
+exports.getAccount = async(event) => {
+    try {
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Account retrieved successfully',
+                input: event,
+            }),
+        };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+        return response;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 };
